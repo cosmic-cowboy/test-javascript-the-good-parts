524b802a0da0826c838023cf0a2484113e90022b
hiroo takaku <slgerkamp@gmail.com>
2013-04-23 22:56:30 +0900

    04_Function4-2

---
+++ 04_Functions/src/add.js
@@ -0,0 +1,5 @@
+// 4.2 add という変数を定義し、２つの値を加算する関数を格納する
+
+var add = function (a,b) {
+	return a + b;
+};
---
+++ 04_Functions/test/add_test.js
@@ -0,0 +1,8 @@
+// add.jsをテスト
+TestCase("add test", {
+
+	"test add a b" : function () {
+		assertEquals(4, add(1,3));
+	}
+
+});
