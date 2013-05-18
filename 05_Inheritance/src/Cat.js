4153c7b3643cfc6b828a3e7d302c26d780769027
hiroo takaku <slgerkamp@gmail.com>
2013-05-18 12:26:10 +0900

    5-1 Pseudoclassical 02 create Pseudoclass

---
+++ 05_Inheritance/src/Cat.js
@@ -0,0 +1,24 @@
+		var Cat = function (name) {
+			this.name = name;
+			this.saying = 'meow';
+		};
+
+		// Cat.prototypeを新しいManmmalのインスタンスで置き換える
+		Cat.prototype = new Mammal();
+
+		// 新しいプロトタイプを追加
+		Cat.prototype.purr = function(n) {
+			var i, s = '';
+			for (i = 0; i < n; i += 1) {
+				if(s){
+					s += '-';
+				}
+				s += 'r';
+			}
+			return s;
+		};
+
+		// メソッドのオーバーライド
+		Cat.prototype.get_name = function() {
+			return this.says() + ' ' + this.name + ' ' + this.says();
+		};
--- 05_Inheritance/test/new_test.js
+++ 05_Inheritance/test/new_test.js
@@ -1,13 +1,28 @@
 TestCase("05_Inheritance Test",{
 
 	// 5.1 Pseudoclassical
-	"test inherited by Object" : function () {
+	"test use new method, inherited by Object" : function () {
 		var newObject = Object.new();
 		assertEquals("object", typeof newObject);
 	},
-	"test inherited by Mammal" : function () {
+
+	"test use new method,  inherited by Mammal" : function () {
 		var sub_Mammal = Mammal.new;
 		assertNotEquals("object", typeof sub_Mammal);
+	},
+
+	"test create instance" : function () {
+		var mammal_instance = new Mammal('Herb the Mammal');
+		assertEquals('Herb the Mammal', mammal_instance.get_name());
+	},
+
+	"test create Pseudoclass" : function () {
+
+		var myCat = new Cat('Henrietta');
+		assertEquals('meow', myCat.says());
+		assertEquals('r-r-r-r-r', myCat.purr(5));
+		assertEquals('meow Henrietta meow', myCat.get_name());
+
 	}
 
 
