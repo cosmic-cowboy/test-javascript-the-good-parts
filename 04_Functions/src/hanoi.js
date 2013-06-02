1697a703cee68ba6fd19a33cddd035070f618fce
hiroo takaku <slgerkamp@gmail.com>
2013-06-02 22:45:36 +0900

    04_Function4-8 Recursion

---
+++ 04_Functions/src/hanoi.js
@@ -0,0 +1,9 @@
+// 4.8 再帰
+
+var hanoi = function (disc, src, aux, dst) {
+	if(disc > 0){
+		hanoi(disc - 1, src, dst, aux);
+		document.writeln('Move disc ' + disc + ' from ' + src + ' to ' + dst);
+		hanoi(disc - 1, aux, src, dst);
+	}
+};
